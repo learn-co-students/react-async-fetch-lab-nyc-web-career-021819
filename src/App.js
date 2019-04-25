5187dbf330bcd65ce6de43f5d945ac1a895f1206
tchang9 <tlchang@gmail.com>
2019-04-25 12:10:36 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React from 'react'
+
+export default class App extends React.Component {
+
+    state = {
+        hi: []
+    }
+
+    render() {        
+        return null
+    }
+
+    componentDidMount() {
+        fetch(`http://api.open-notify.org/astros.json`)
+        .then(res => res.json())
+        .then(data => {
+            this.setState({
+                hi: data.people
+            })
+        })
+    }
+}

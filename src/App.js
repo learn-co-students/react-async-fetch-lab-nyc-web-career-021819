39b6be96900dd12a5e34a9db3bb9e7641da82690
rrosztoczy <rrosztoczy@gmail.com>
2019-04-25 10:50:08 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import React, { Component } from 'react'
+
+export default class App extends Component {
+
+    state = {
+        apiResponse: []
+    }
+
+    componentDidMount() {
+        (async () => {
+        const resp = await fetch('http://api.open-notify.org/astros.json')
+        const jsonResp = await resp.json()
+        this.setState({apiResponse: jsonResp})
+        })()
+    }
+
+    render() {
+
+        return(<div>{JSON.stringify(this.state.apiResponse)}</div>)
+    }
+}
